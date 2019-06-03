f644a4ae8513133ea97a19a59c08a6ae391baf05
Sander Ruscigno <ruscigno@gmail.com>
2019-06-03 19:49:05 -0300

    chore: initial commit

---
+++ backend/src/main/java/com/ruscigno/UpcomingMoviesWebAppbackend/UpcomingMoviesWebAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ruscigno.UpcomingMoviesWebAppbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpcomingMoviesWebAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpcomingMoviesWebAppBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ruscigno/UpcomingMoviesWebAppbackend/UpcomingMoviesWebAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruscigno.UpcomingMoviesWebAppbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpcomingMoviesWebAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
